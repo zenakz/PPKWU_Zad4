0789800330abd43af2b7869cb0fe4b7885069ccf
Zientak.David <Zenakz@github.com>
2020-12-15 15:53:43 +0100

    added address class

---
+++ zad4/src/main/java/com/example/zad4/Address.java
@@ -0,0 +1,31 @@
+package com.example.zad4;
+
+public class Address {
+    String streetAddress;
+    String addressLocality;
+    String addressCountry;
+
+    public String getStreetAddress() {
+        return streetAddress;
+    }
+
+    public void setStreetAddress(String streetAddress) {
+        this.streetAddress = streetAddress;
+    }
+
+    public String getAddressLocality() {
+        return addressLocality;
+    }
+
+    public void setAddressLocality(String addressLocality) {
+        this.addressLocality = addressLocality;
+    }
+
+    public String getAddressCountry() {
+        return addressCountry;
+    }
+
+    public void setAddressCountry(String addressCountry) {
+        this.addressCountry = addressCountry;
+    }
+}
