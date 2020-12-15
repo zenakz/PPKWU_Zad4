b17ab3952f35c0ad8d637f29325d3ad0d57f9dea
Zientak.David <Zenakz@github.com>
2020-12-15 14:52:30 +0100

    added jsoup and basic endpoints

--- zad4/src/main/java/com/example/zad4/Controller.java
+++ zad4/src/main/java/com/example/zad4/Controller.java
@@ -1,7 +1,21 @@
 package com.example.zad4;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 public class Controller {
+
+    @GetMapping("/search")
+    String getSearchResults(String searchString, HttpServletResponse response){
+        String url = "https://panoramafirm.pl/szukaj?k="+searchString;
+        return url;
+    }
+    
+    @GetMapping("/card")
+    void returnCard(HttpServletResponse response){
+
+    }
 }
